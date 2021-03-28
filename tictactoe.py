2fa0f7972f2fdc56b7769aa54a31e61f7db3c668
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2021-03-28 15:39:37 +0100

    implemented all tictactoe methods except for the minimax algorithm

--- tictactoe.py
+++ tictactoe.py
@@ -22,42 +22,97 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    flattened_board = sum(board, [])
+    count_x = flattened_board.count(X)
+    count_o = flattened_board.count(O)
+
+    if count_o < count_x:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = []
+    i = 0
+    while i < 3:
+        j = 0
+        while j < 3:
+            if board[i][j] == EMPTY:
+                possible_actions.append((i, j))
+            j += 1
+        i += 1
+
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    current_player = player(board)
+
+    board[action[0]][action[1]] = current_player
+
+    return board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+
+    for line in board:
+        if all_the_same(line):
+            return line[0]
+
+    diagonals = [[board[0][0], board[1][1], board[2][2]],
+                 board[0][2], board[1][1], board[2][0]]
+    for diagonal in diagonals:
+        if all_the_same(diagonal):
+            return diagonal[0]
+
+    columns = [[board[0][0], board[1][0], board[2][0]],
+               [board[0][1], board[1][1], board[2][1]],
+               [board[0][2], board[1][2], board[2][2]]]
+
+    for column in columns:
+        if all_the_same(column):
+            return column[0]
+
+
+def all_the_same(list):
+        if len(set(list)) <= 1:
+            return True
+        else:
+            return False
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    flattened_board = sum(board, [])
+    if flattened_board.count(EMPTY) >= 1:
+        return False
+    else:
+        return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner_player = winner(board)
+    if winner_player == X:
+        return 1
+    elif winner_player == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
